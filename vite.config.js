4a933dbb361d3bbd52899359407d632ede2dcc87
Thomas Scales <thomasscales@Thomass-Mac-mini.local>
2025-04-05 09:37:40 -0400

    Set dynamic base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/aussie-gut-pack/",
+  base: process.env.VITE_BASE_PATH || "/",
   plugins: [react(), tailwindcss()],
 });
