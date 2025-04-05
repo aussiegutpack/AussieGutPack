5adbb706e78e6b488ff9796a98c51b5a6235c814
Thomas Scales <thomasscales@Thomass-Mac-mini.local>
2025-04-05 10:46:24 -0400

    new .env files so we can make changes to both GH pages and Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import dotenv from "dotenv";
+import path from "path";
 
+// Load the appropriate .env file based on the mode
+const mode = process.argv.includes("--mode")
+  ? process.argv[process.argv.indexOf("--mode") + 1]
+  : "development";
+const envFile =
+  mode === "github"
+    ? ".env.github"
+    : mode === "vercel"
+    ? ".env.vercel"
+    : ".env";
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
+
+console.log("VITE_BASE_PATH:", process.env.VITE_BASE_PATH);
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.VITE_BASE_PATH || "/",
