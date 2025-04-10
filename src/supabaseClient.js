5a6badbbcf4b49767719000b4a57fa4c6065e908
Thomas Scales <thomasscales@Thomass-Mac-mini.local>
2025-04-10 08:31:34 -0400

    Trying to add some console.log to see if it even loads on vercel

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -5,5 +5,13 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 console.log("Supabase URL:", supabaseUrl); // Debug log
 console.log("Supabase Anon Key:", supabaseAnonKey); // Debug log
+if (!supabaseUrl) {
+  throw new Error("VITE_SUPABASE_URL is not defined in environment variables");
+}
+if (!supabaseAnonKey) {
+  throw new Error(
+    "VITE_SUPABASE_ANON_KEY is not defined in environment variables"
+  );
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
